b07cc1e8a8c5747e97a876f1aa9b7c67fddc0488
Master Zohaib <120156969+realzohaib30@users.noreply.github.com>
2022-12-26 08:54:01 -0800

    Add files via upload

---
+++ task9dog.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+cout << "   -----------------------------------                       "<<endl;
+cout << "      o   ^ ___ ^                                            "<<endl; 
+
+cout << "       o  ( oo ) \\___________                               "<<endl;
+
+cout << "          (____)\\            )\\/\\                         "<<endl;                                                                               
+
+cout << "                 ||---------w |                              "<<endl;
+cout << "                 ||          ||                              "<<endl;
+                                             
+         
+  }  
+
+
+                             
